a32d3a3357e05cf40695c232d7796c2cca8d14ef
Sip <sipg789@gmail.com>
2022-06-09 21:18:47 -0500

    created user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,51 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+
+
+const User = model('User', UserSchema);
+
+UserSchema.virtual('frienCount').get(function() {
+    return this.friends.length;
+});
+
+
+module.exports = User;
